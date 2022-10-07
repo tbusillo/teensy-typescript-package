3053378af5a1ff2d16e51de80fe58b5952b75232
Tom <thomas.j.busillo@gmail.com>
2022-10-07 00:47:49 -0400

    another try

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # WIP - teensy-typescript-package
+[![.github/workflows/test.yml](https://github.com/tbusillo/teensy-typescript-package/actions/workflows/test.yml/badge.svg?event=workflow_run)](https://github.com/tbusillo/teensy-typescript-package/actions/workflows/test.yml)
 
 A teensy tiny TypeScript package template. 
 ## Features
+- TypeScript 4.8+
 - native ESM support
-- unbuild - bundless bundler
+- Rollup/unbuild - bundless bundler
 - jest/ts-jest
 - eslint/prettier/git hooks
 - CI/CD via GH Actions
---
+++ build.config.ts
@@ -0,0 +1,21 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  // If entries is not provided, will be automatically inferred from package.json
+  entries: [
+    // default
+    "./src/index",
+    // mkdist builder transpiles file-to-file keeping original sources structure
+    {
+      builder: "mkdist",
+      input: "./src/package/components/",
+      outDir: "./build/components"
+    }
+  ],
+
+  // Change outDir, default is 'dist'
+  outDir: "build",
+
+  // Generates .d.ts declaration file
+  declaration: true
+});
