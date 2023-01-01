cb6293022e94f96470fed0edbdba155e20571262
Tom <tbusillo@users.noreply.github.com>
2023-01-01 01:30:17 -0500

    chore: update dependencies and other minor improvements

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,30 @@
-export default {
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true
-      }
-    ]
-  }
+  moduleDirectories: ['node_modules', 'src'],
+  // moduleNameMapper: {
+  //   '@teensy/uuid': '<rootDir>/node_modules/@teensy/uuid/dist/index.cjs'
+  // },
+  resolver: 'ts-jest-resolver'
+  // transform: {
+  //   //   // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+  //   '^.+\\.m?[tj]sx?$': [
+  //     'ts-jest',
+  //     {
+  //       useESM: true
+  //     }
+  //   ]
+  // } //to process js/ts/mjs/mts with `ts-jest`
+  //   '^.+\\.tsx?$': [
+  //     'ts-jest',
+  //     {
+  //       useESM: true
+  //     }
+  //   ]
+  // }
 }
+
+export default jestConfig
